c77fb7dee78f159c2337e692c4798443a4e0d6ee
ARBUCHELI <radio_seattle@hotmail.com>
2025-03-14 11:50:44 +0300

    implementing few shot prompting

---
+++ INTRODUCTION TO LANGCHAIN & CHATBOT MECHANICS/implementing-few-shot-prompting/script.py
@@ -0,0 +1,39 @@
+from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
+from langchain_huggingface import HuggingFacePipeline
+
+# Create the examples list of dicts
+examples = [
+  {
+    "question": "How many DataCamp courses has Jack completed?",
+    "answer": "36"
+  },
+  {
+    "question": "How much XP does Jack have on DataCamp?",
+    "answer": "284,320XP"
+  },
+  {
+    "question": "What technology does Jack learn about most on DataCamp?",
+    "answer": "Python"
+  }
+]
+
+# Complete the prompt for formatting answers
+example_prompt = PromptTemplate.from_template("Question: {question}\n{answer}")
+
+# Create the few-shot prompt
+prompt_template = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    suffix="Question: {input}",
+    input_variables=["input"],
+)
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="crumb/nano-mistral",
+    task="text-generation",
+    pipeline_kwargs={"max_new_tokens": 20}
+)
+
+llm_chain = prompt_template | llm
+
+print(llm_chain.invoke({"input": "What is Jack's favorite technology on DataCamp?"}))
